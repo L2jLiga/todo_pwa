752688db5b99f6cb11e623cbf1edea7b9826a971
SIDENIS\Andrey.Chalkin <andrey.chalkin@sidenis.com>
2018-11-23 12:06:56 +0700

    upd

---
+++ docs/precache-manifest.abf21dd8a8397c667ccb642c539f7eae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89006d5ef47f4b159aed",
+    "url": "/todo_pwa/static/js/runtime~main.89006d5e.js"
+  },
+  {
+    "revision": "28f9f2ca1361dccc863a",
+    "url": "/todo_pwa/static/js/main.28f9f2ca.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/todo_pwa/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "28f9f2ca1361dccc863a",
+    "url": "/todo_pwa/static/css/main.36cf6170.chunk.css"
+  },
+  {
+    "revision": "0fdac933a74e3f9288a17707fcf13009",
+    "url": "/todo_pwa/index.html"
+  }
+];
