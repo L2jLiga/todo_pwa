aa1de10d5beaa0ad08a145fceacd655c52a95105
SIDENIS\Andrey.Chalkin <andrey.chalkin@sidenis.com>
2018-11-23 12:53:44 +0700

    Updated docs

--- public/service-worker-custom.js
+++ public/service-worker-custom.js
@@ -1,7 +1,11 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
     '/todo_pwa/',
-    '/todo_pwa/404.html'
+    '/todo_pwa/404.html',
+    '/todo_pwa/static/css/main.chunk.css',
+    '/todo_pwa/static/js/1.chunk.js',
+    '/todo_pwa/static/js/main.chunk.js',
+    '/todo_pwa/static/js/runtime~main.js'
 ];
 
 self.addEventListener('install', function (event) {
