5a684a6eb81e875f839a134a4ba34081ea5c9487
SIDENIS\Andrey.Chalkin <andrey.chalkin@sidenis.com>
2018-11-23 12:01:54 +0700

    upd

---
+++ docs/build/precache-manifest.ad2489ecaa60320c90223dcad4b7e90b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "487ff322e94443df1ec4",
+    "url": "/static/js/main.487ff322.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "487ff322e94443df1ec4",
+    "url": "/static/css/main.36cf6170.chunk.css"
+  },
+  {
+    "revision": "9dc0e9532d3fbf0f0fd971c5ab3e66b8",
+    "url": "/index.html"
+  }
+];
